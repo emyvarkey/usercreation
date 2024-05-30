c10d2a3e4b6486dfd009fa8b67eb9cb7681833e6
emyvarkey <emy.varkey@gmail.com>
2024-05-30 11:14:49 -0500

    update creeateUser method add @service annotation in service class

--- src/main/java/com/example/usercreation/service/UserService.java
+++ src/main/java/com/example/usercreation/service/UserService.java
@@ -3,9 +3,10 @@ package com.example.usercreation.service;
 import com.example.usercreation.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
     public void saveUser(User user) {
